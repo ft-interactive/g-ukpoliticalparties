d08d18de027dd060bc794901c200f9377e98f51a
tomgp <tom.g.pearson@gmail.com>
2017-05-02 16:02:21 +0100

    add colours

--- index.js
+++ index.js
@@ -1,11 +1,105 @@
-var data = [
-	{name:'Conservatives', shortname:'Con', ft_id:'c', pa_id:''},
-	{name:'Labour', shortname:'Lab', ft_id:'l', pa_id:''},
-	{name:'Liberal Democrats', shortname:'LD', ft_id:'ld', pa_id:''},
-	{name:'UKIP', shortname:'UKIP', ft_id:'ukip', pa_id:''},
-	{name:'Greens', shortname:'Green', ft_id:'g', pa_id:''},
-	{name:'SNP', shortname:'SNP', ft_id:'snp', pa_id:''},
-];
+var fills = require('g-chartcolour').ukPoliticalParties;
+var lines = require('g-chartcolour').ukPoliticalPartiesText;
+
+var data = [{
+  name:'Conservatives',
+  shortname:'Con',
+  ft_id:'c',
+  pa_id:'',
+  fillColour:fills.conservative,
+  lineColour:lines.conservative
+},{
+  name:'Labour',
+  shortname:'Lab',
+  ft_id:'l',
+  pa_id:'',
+  fillColour:fills.labour,
+  lineColour:lines.labour
+},{
+  name:'Liberal Democrats',
+  shortname:'LD',
+  ft_id:'ld',
+  pa_id:'',
+  fillColour:fills['liberal democrats'],
+  lineColour:lines['liberal democrats']
+},{
+  name:'UKIP',
+  shortname:'UKIP',
+  ft_id:'ukip',
+  pa_id:'',
+  fillColour:fills.ukip,
+  lineColour:lines.ukip
+},{
+  name:'Greens',
+  shortname:'Green',
+  ft_id:'g',
+  pa_id:'',
+  fillColour:fills.green,
+  lineColour:lines.green
+},{
+  name:'SNP',
+  shortname:'SNP',
+  ft_id:'snp',
+  pa_id:'',
+  fillColour:fills.snp,
+  lineColour:lines.snp
+},{
+  name:'DUP',
+  shortname:'DUP',
+  ft_id:'dup',
+  pa_id:'',
+  fillColour:fills.dup,
+  lineColour:lines.dup
+},{
+  name:'Sinn Féin',
+  shortname:'SF',
+  ft_id:'sf',
+  pa_id:'',
+  fillColour:fills.sf,
+  lineColour:lines.sf
+},{
+  name:'Plaid Cymru',
+  shortname:'PC',
+  ft_id:'pc',
+  pa_id:'',
+  fillColour:fills.pc,
+  lineColour:lines.pc
+},{
+  name:'Alliance Party',
+  shortname:'alliance',
+  ft_id:'a',
+  pa_id:'',
+  fillColour:fills.alliance,
+  lineColour:lines.alliance
+},{
+  name:'Independent',
+  shortname:'Ind',
+  ft_id:'i',
+  pa_id:'',
+  fillColour:fills.independent,
+  lineColour:lines.independent
+},{
+  name:'Other',
+  shortname:'oth',
+  ft_id:'oth',
+  pa_id:'',
+  fillColour:fills.other,
+  lineColour:lines.other
+},{
+  name:'SDLP',
+  shortname:'SDLP',
+  ft_id:'sdlp',
+  pa_id:'',
+  fillColour:fills.sdlp,
+  lineColour:lines.sdlp
+},{
+  name:'Ulster Unionist',
+  shortname:'UUP',
+  ft_id:'uup',
+  pa_id:'',
+  fillColour:fills.uup,
+  lineColour:lines.uup
+}];
 
 module.exports = function (str){
   return data.find(function(party){
